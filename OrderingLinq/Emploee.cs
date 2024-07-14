94a203b0ad230cf497020fc5420c661f5a462064
HariDhake <dhakedharish6216@gmail.com>
2024-07-14 13:31:11 +0530

    added employee

---
+++ OrderingLinq/Emploee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingLinq
+{
+    public class Emploee
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+}
