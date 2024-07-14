67c70012e8b9073566c31a4963de351a989f8b19
HariDhake <dhakedharish6216@gmail.com>
2024-07-14 13:27:54 +0530

    Add project files.

---
+++ OrderingLinq/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List of Students before Sorting");
+            /*List<Student> students = Student.GetStudents();
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.Name);
+                Console.WriteLine(student.StudentID);
+                Console.WriteLine(student.TotalMarks);
+            }*/
+            IOrderedEnumerable<Student> inum=  Student.GetStudents().OrderBy(x => x.TotalMarks);
+            foreach (Student student in inum)
+            {
+                Console.WriteLine(student.Name);
+                Console.WriteLine(student.StudentID);
+                Console.WriteLine(student.TotalMarks);
+
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OrderingLinq/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingLinq
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public String Name { get; set; }
+        public int TotalMarks { get; set; }
+        public static List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>
+        {
+            new Student { StudentID = 1, Name = "Harish" ,TotalMarks=80},
+            new Student { StudentID = 2, Name = "Adtiya" ,TotalMarks=120},
+            new Student { StudentID = 3, Name = "Priyanka" ,TotalMarks=150},
+            new Student { StudentID = 4, Name = "Manish" ,TotalMarks=200},
+            new Student { StudentID = 5, Name = "Madhuri" ,TotalMarks=2500},
+            new Student { StudentID = 6, Name = "Pravin" ,TotalMarks=500},
+        };
+            return students;
+        }
+    }
+}
